8f59e92d47e296293b4660e0c4b8e4f261c331bc
Yevhen Letin <Formacion03@CADIT.spain>
2022-12-16 13:48:18 +0100

    crear Employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  employeeArray: Employee[] = [
+    {id: 1, name: 'Max Mustermann', country: 'Germany'},
+    {id: 2, name: 'Ryan', country: 'Spain'},
+    {id: 3, name: 'Joe', country: 'USA'},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    this.selectedEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+  
+
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,20 @@
+export class Employee {
+    id: number;
+    name: string;
+    country: string;
+/*
+    constructor(_id:number, _name:string, _country:string) {
+            this.id = _id;
+            this.name = _name;
+            this.country = _country;
+      
+    }
+*/
+
+constructor() {
+    this.id = 0
+    this.name = ""      
+    this.country = ""
+
+}
+}
